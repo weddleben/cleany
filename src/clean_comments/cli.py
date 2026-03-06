426a2e436f8c19944dffeda038f253d94c2ba54d
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2026-03-05 21:10:29 -0600

    change name

--- src/clean_comments/cli.py
+++ src/clean_comments/cli.py
@@ -117,7 +117,7 @@ def main():
     args = parse_args()
 
     if len(sys.argv) == 1:
-        return print("clean -h for help")
+        return print("cleany -h for help")
 
     if args.path:
         path = args.path
