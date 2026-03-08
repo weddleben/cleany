2df7cb71fa22db411212b9c4bd2aff6c8388dbff
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2026-03-07 18:20:42 -0600

    add tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import subprocess
 
@@ -6,15 +7,37 @@ import pytest
 def test_init_1():
     '''if nothing passed in, should return default help message'''
     output = subprocess.run("cleany", capture_output=True)
-    print(output.stdout)
     assert "help" in str(output.stdout)
 
 def test_init_2():
+    '''runs...'''
     subprocess.run(["cleany", "--nuke"])
 
 def test_init_3():
+    '''wrong type passed to path. should be str'''
     with pytest.raises(Exception):
         subprocess.run(["cleany", "--path", 2])
+
+def test_init_4():
+    '''if no modification commands passed (emoji, nuke, etc)'''
+    output = subprocess.run(["cleany", "--path", "some_dir"], capture_output=True)
+    assert "no modification commands" in str(output.stdout)
+
+def test_init_5():
+    '''if no modification commands passed (emoji, nuke, etc)'''
+    output = subprocess.run(["cleany", "--ignore-dir", "some_dir"], capture_output=True)
+    assert "no modification commands" in str(output.stdout)
+
+def test_init_6():
+    '''passing in non-existent directory'''
+    output = subprocess.run(["cleany", "--path", "doesnt_exist", "--nuke"], capture_output=True)
+    assert "cannot find matchign directory" in str(output.stdout)
+
+def test_init_7(tmp_path):
+    '''specifying directory with no matching python files'''
+    output = subprocess.run(["cleany", "--path", tmp_path, "--nuke"], capture_output=True)
+    print(tmp_path)
+    assert "no files found in" in str(output.stdout)
 
 def test_remove_emojis_1(tmp_path):
     pre = Path("tests/fixtures/pre-clean-emojis.py")
