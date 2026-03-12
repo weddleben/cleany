2bd216d302471b255ff713f7dab512d5d69dabdd
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2026-03-11 20:15:01 -0500

    typo smh

--- src/cleany/cleany.py
+++ src/cleany/cleany.py
@@ -27,7 +27,7 @@ class Cleany(BaseModel):
         if not any([self.nuke, self.emoji]):
             return print("no modification commands")
         if not self.path.exists():
-            return print(f"cannot find matchign directory: {self.path}")
+            return print(f"cannot find matching directory: {self.path}")
         if len(self.list_of_files) == 0:
             return print(f"no files found in {self.path}")
         for file in self.list_of_files:
